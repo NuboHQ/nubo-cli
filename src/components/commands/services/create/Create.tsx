482262447be0399ec34ea526774a6d288226abe5
Nubo Developer <developer@nubo.app>
2021-10-06 12:02:06 +0100

    Started create service command

--- src/components/commands/services/create/Create.tsx
+++ src/components/commands/services/create/Create.tsx
@@ -7,58 +7,64 @@ import { getTypeNameIdFromCli } from './utils';
 import Help from './help/Help';
 
 const Create: FC<CommandComponent> = ({ cli }) => {
-	const typeNameId = getTypeNameIdFromCli(cli);
-	const [loading, setLoading] = useState(true);
-	const [error, setError] = useState('');
+  const typeNameId = getTypeNameIdFromCli(cli);
+  const [loading, setLoading] = useState(true);
+  const [complete, setComplete] = useState(false);
+  const [error, setError] = useState('');
 
-	const test = useCallback(async () => {
-		try {
-			const query = `
-				query {
-					services {
-						items {
-							nameId
-						}
+  const test = useCallback(async () => {
+    try {
+      const query = `
+				mutation AddService($data: AddServiceDataInput!) {
+					addService(data: $data) {
+						id
+						nameId
 					}
 				}
 			`;
+      const variables = {
+        data: {
+          nameId: 'from-cli',
+          type: { type: 'products', nameId: typeNameId },
+        },
+      };
 
-			const result = await data({ query });
-			result.data.services.items.map((service: any) => {
-				return {
-					name: service.nameId,
-				};
-			});
-			setLoading(false);
-		} catch (error: any) {
-			setError(error.message || 'Unknown error');
-		}
-	}, []);
+      await data({ query, variables });
+      setComplete(true);
+      setLoading(false);
+    } catch (error: any) {
+      setError(error.message || 'Unknown error');
+    }
+  }, []);
 
-	useEffect(() => {
-		test();
-	}, []);
+  useEffect(() => {
+    test();
+  }, []);
 
-	if (!typeNameId) {
-		return <Text color="red">Missing type (i.e. node, postgres)</Text>;
-	}
+  if (!typeNameId) {
+    return <Text color="red">Missing type (i.e. node, postgres)</Text>;
+  }
 
-	if (error) {
-		return <Text color="red">{error}</Text>;
-	}
+  if (error) {
+    return <Text color="red">{error}</Text>;
+  }
 
-	if (loading) {
-		return (
-			<Text>
-				<Text color="blue">
-					<Spinner type="dots" />
-				</Text>
-				{' Loading'}
-			</Text>
-		);
-	}
+  if (loading) {
+    return (
+      <Text>
+        <Text color="blue">
+          <Spinner type="dots" />
+        </Text>
+        {' Loading'}
+      </Text>
+    );
+  }
 
-	return <Text>Create {typeNameId}</Text>;
+  if (complete) {
+    return <Text>Created from-cli</Text>;
+  }
+
+  return null;
 };
 
 export { Create, Help };
